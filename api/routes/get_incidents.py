3112ba0df19604acca3e81c623713514a21d9f6e
kalule Arthur <kalulearthur@gmail.com>
2018-12-27 15:36:33 +0300

    Reduce complexity of get redflag endpoints
    [#162814389]
     On branch reduce-complexity
     Changes to be committed:
    	modified:   .coverage
    	modified:   api/app.py
    	new file:   api/routes/get_incidents.py
    	modified:   api/routes/incidents.py

---
+++ api/routes/get_incidents.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, jsonify
+
+from api.helpers.auth_token import token_required
+
+get_red_flags_bp = Blueprint(
+    "get_red_flags_bp", __name__, url_prefix="/api/v1"
+)
+from api.models.incident import (
+    red_flags,
+    get_incident_record,
+    get_all_incident_records,
+)
+
+
+@get_red_flags_bp.route("/red-flags", methods=["GET"])
+@token_required
+def get_all_red_flags():
+    return (
+        jsonify({"status": 200, "data": get_all_incident_records(red_flags)}),
+        200,
+    )
+
+
+@get_red_flags_bp.route("/red-flags/<red_flag_id>", methods=["GET"])
+@token_required
+def get_a_red_flag(red_flag_id):
+    try:
+        red_flag_id = int(red_flag_id)
+    except ValueError:
+        return (
+            jsonify(
+                {"status": 400, "error": "Red-flag id must be an integer"}
+            ),
+            400,
+        )
+
+    results = get_incident_record(red_flag_id, red_flags)
+    response = None
+    if results:
+        response = jsonify({"status": 200, "data": [results]}), 200
+    else:
+
+        response = (
+            jsonify(
+                {"status": 404, "error": "Red-flag record does not exist"}
+            ),
+            404,
+        )
+
+    return response
--- api/routes/incidents.py
+++ api/routes/incidents.py
@@ -11,21 +11,13 @@ from api.models.incident import (
     RedFlag,
     incident_record_exists,
     red_flags,
-    get_incident_record,
-    get_all_incident_records,
-    get_incident_obj_by_id,
 )
 from api.helpers.validation import (
     validate_new_incident,
-    is_valid_id,
-    validate_edit_location,
-    request_data_required,
-    validate_comment,
 )
 
 from api.helpers.responses import (
     expected_new_incident_format,
-    delete_not_allowed,
 )
 
 
@@ -92,34 +84,3 @@ def new_red_flag():
         )
 
     return response
-
-
-@red_flags_bp.route("/red-flags", methods=["GET"])
-@token_required
-def get_all_red_flags():
-    return (
-        jsonify({"status": 200, "data": get_all_incident_records(red_flags)}),
-        200,
-    )
-
-
-@red_flags_bp.route("/red-flags/<red_flag_id>", methods=["GET"])
-@token_required
-def get_a_red_flag(red_flag_id):
-    try:
-        red_flag_id = int(red_flag_id)
-    except ValueError:
-        return (
-            jsonify(
-                {"status": 400, "error": "Red-flag id must be an integer"}
-            ),
-            400,
-        )
-
-    results = get_incident_record(red_flag_id, red_flags)
-    if results:
-        return jsonify({"status": 200, "data": [results]}), 200
-    return (
-        jsonify({"status": 404, "error": "Red-flag record does not exist"}),
-        404,
-    )
