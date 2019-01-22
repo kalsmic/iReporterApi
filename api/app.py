e21111038bbdd9aa222730c9d724e7b56d4a5ea0
Kalule <kalulearthur@gmail.com>
2019-01-22 23:01:06 +0300

    Remove endpoints with non persistent data [#163355518]

--- api/app.py
+++ api/app.py
@@ -2,12 +2,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-from api.helpers.responses import supported_end_points
-from api.routes.auth import users_bp
-from api.routes.create_incident import create_red_flags_bp
-from api.routes.delete_incident import delete_red_flag_bp
-from api.routes.edit_incident import edit_red_flags_bp
-from api.routes.get_incidents import get_red_flags_bp
 from instance.config import Config
 
 
@@ -35,7 +29,7 @@ def create_app(config=None):
             jsonify(
                 {
                     "error": "Bad JSON format data",
-                    "status":400
+                    "status": 400
                 }
             ),
             400,
@@ -45,30 +39,17 @@ def create_app(config=None):
     def _page_not_found(e):
         return (
             jsonify(
-                {
-                    "error": "Endpoint for specified URL does not exist",
-                    "supportedEndPoints": supported_end_points,
-                }
+                {"error": "Endpoint for specified URL does not exist"}
             ),
             404,
         )
+
     @app.errorhandler(405)
     def _method_not_allowed(e):
         return (
-            jsonify(
-                {
-                    "error": "Method not allowed",
-                    "supportedMethods": supported_end_points
-                }
-            ),
+            jsonify({"error": "Method not allowed"}),
             405,
         )
 
     app.config.from_object(Config)
-    app.register_blueprint(users_bp)
-    app.register_blueprint(create_red_flags_bp)
-    app.register_blueprint(delete_red_flag_bp)
-    app.register_blueprint(edit_red_flags_bp)
-    app.register_blueprint(get_red_flags_bp)
-
     return app
