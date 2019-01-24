54f51efc51e648124088571ef8c59437dc696056
Kalule <kalulearthur@gmail.com>
2019-01-24 06:01:41 +0300

    Add Get interventions endpoint  [#163397538]

--- tests/test_get_incidents.py
+++ tests/test_get_incidents.py
@@ -83,9 +83,30 @@ def test_admin_get_a_red_flag(client):
     )
 
 
-def test_get_a_red_flag_with_invalid_id(client):
-    response = client.get("api/v2/red-flags/fs", headers=user2_header)
-    assert response.status_code == 400
+# INTERVENTIONS
+
+def test_get_all_interventions_without_token(client):
+    # test only logged in user get red flags
+    response = client.get("api/v2/interventions")
+    assert response.status_code == 401
     data = json.loads(response.data.decode())
-    assert data["status"] == 400
-    assert data["error"] == "Invalid incident id"
+    assert data == {"error": invalid_token_message, "status": 401}
+
+
+def test_get_all_interventions_with_expired_token(client):
+    # test only logged in user get red flags
+    response = client.get("api/v2/interventions", headers=expired_token_header)
+    assert response.status_code == 401
+    data = json.loads(response.data.decode())
+    assert data == {"error": expired_token_message, "status": 401}
+
+
+def test_get_all_interventions(client):
+    response = client.get("api/v2/interventions", headers=user1_header)
+    assert response.status_code == 200
+    data = json.loads(response.data.decode())
+    assert data["data"][0]["created_by"] == user1_id
+
+    response = client.get("api/v2/interventions", headers=user2_header)
+    assert response.status_code == 200
+    assert isinstance(data["data"],list)
