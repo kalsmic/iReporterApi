3d82052c1738b1aaf651229a49bd4ce4f542a1ce
Kalule <kalulearthur@gmail.com>
2019-02-04 18:28:25 +0300

    Add input validation with JAVASCRIPT [#162623351] [#162630485]

--- UI/static/js/create_incident.js
+++ UI/static/js/create_incident.js
@@ -1,8 +1,79 @@
+function displayError(dataArray) {
+
+    for (let key in dataArray) {
+
+        if ({}.hasOwnProperty.call(dataArray, key)) {
+            let fieldError = document.getElementById(key + "-error");
+            fieldError.style.display = "block";
+            fieldError.innerHTML = dataArray[key];
+        }
+    }
+}
+
 let authorizationHeader = "Bearer ".concat(localStorage.getItem("token"));
 
 let incidentType = document.getElementById("incident_type");
 let incidentTitle = document.getElementById("incident_title");
-let incidentComment = document.getElementById("description");
+let incidentComment = document.getElementById("comment");
+let incidentlocation = document.getElementById("set_location");
+let locationError = document.getElementById("location-error");
+
+incidentTitle.onkeyup = function () {
+    let titleError = document.getElementById('title-error');
+
+    if (incidentTitle.value.length < 4) {
+        titleError.style.display = "block";
+        titleError.innerHTML = "Title must contain atleast 4 characters";
+        incidentTitle.setCustomValidity("Invalid record title.");
+
+
+    } else if (incidentTitle.value.length > 100) {
+        titleError.style.display = "block";
+        titleError.innerHTML = "Title can not contain more than 100 characters";
+        incidentTitle.setCustomValidity("Invalid record title.");
+
+    } else {
+        titleError.style.display = "none";
+        incidentTitle.setCustomValidity("");
+
+    }
+
+
+};
+
+incidentComment.onkeyup = function () {
+    let commentError = document.getElementById('comment-error');
+    if (incidentComment.value.trim().length < 20) {
+        incidentComment.setCustomValidity("Invalid comment.");
+        commentError.style.display = "block";
+        commentError.innerHTML = "Comment should have at least 20 characters";
+    } else {
+        commentError.style.display = "none";
+        incidentComment.setCustomValidity("");
+    }
+};
+
+incidentComment.onblur = function () {
+
+    if (incidentlocation.value === "") {
+        locationError.style.display = "block";
+        locationError.innerHTML = "Please pick a location from the map";
+        incidentlocation.setCustomValidity("Invalid location Coordinates.");
+
+    } else {
+        locationError.style.display = "none";
+        incidentlocation.setCustomValidity("");
+
+    }
+
+};
+
+document.getElementById('googleMap').onclick = function () {
+    locationError.style.display = "none";
+    incidentlocation.setCustomValidity("");
+
+};
+
 
 function createIncident() {
 
@@ -27,14 +98,11 @@ function createIncident() {
         .then((data) => {
             if (data.status === 400) {
                 //Bad format data
-                // displayError(data.error);
-                alert(JSON.stringify(data.error));
+                displayError(data.error);
 
 
             } else if (data.status === 401) {
-                //Duplicate username, email or phone number
-                // displayError(data.error);
-                alert("unat");
+                displayError(data.error);
                 window.setTimeout(function () {
                     window.location.replace("../index.html");
                 }, 5000);
@@ -44,7 +112,6 @@ function createIncident() {
                 //on success
                 let newRecord = data["data"][0][incidentType.value];
                 let successMsg = data["data"][0]["success"];
-                console.log(newRecord);
                 let newRecordDetails = `
                        <h3 class="text-green">Successfully ${successMsg} !</h3>
 
