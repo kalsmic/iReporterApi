14e5297dd31d4d1eea7dbddcbce920ed82ad6937
Kalule <kalulearthur@gmail.com>
2019-02-18 18:31:07 +0300

    Add page to diplay a list of users
    [#164049287]

--- UI/static/js/statistics.js
+++ UI/static/js/statistics.js
@@ -67,10 +67,11 @@ fetch(url, {
                         <div class="dash-stat bg-red">${incidents['interventions']['rejected']}</div>
                         <div>Rejected</div>
                     </div>
+                  
                 </div>
 
             `;
-            document.getElementById('user_dash_info').innerHTML = `
+            let userInfo = `
                 <img src="../static/img/profile-pics/user1.png" class="img-profile" alt="profile picture">
                 <p>Username: ${sessionStorage.getItem('iRUsername')}</p>
                 <p>First Name: ${sessionStorage.getItem('iRFirstName')}</p>
@@ -78,8 +79,22 @@ fetch(url, {
                 <p id="other_names">Other Names: ${sessionStorage.getItem('iROtherNames')}</p>
                 <p>Tel: ${sessionStorage.getItem('iRPhoneNumber')}</p>
                 <p>Email: ${sessionStorage.getItem('iREmail')}</p>
-                              
-            `;
+                `;
+                if("users" in incidents === true) {
+                    userInfo += `
+                    <h3 class="text-blue">
+                        <i class="fa fa-users text-lblue" aria-hidden="true">&nbsp;</i>
+                            Users 
+                    </h3>
+                    <a href="../user/index.html">
+                    <div class="dash bg-lblue text-blue">
+                        <div class="dash-stat bg-lblue">${incidents['users']}</div>
+                        <div class="bg-purple">Users</div>
+                    </div></a>`;
+                }
+                          
+
+            document.getElementById('user_dash_info').innerHTML = userInfo;
 
             if (!sessionStorage.getItem('iROtherNames')) {
                 document.getElementById('other_names').style.display = 'none';
